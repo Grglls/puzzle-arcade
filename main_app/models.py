25059291357fe2bfcd684ade1328826703e70fec
Greg Ells <gregells@gmail.com>
2024-01-29 21:57:36 +1300

    Favicon added.

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
+
+
+GAMES = (
+    ('Hangman', 'https://grglls.github.io/Hangman/'),
+    ('Word Guesser', 'https://grglls.github.io/Hangman/'),
+)
 
 # Create your models here.
